8b3902af46b98eb3bfe8966e1f7f2d4ec3f0805f
admin <admin@TeamCity>
2026-07-29 13:14:30 +0000

    triggers of 'Build' build configuration were updated (TeamCity change in 'example-teamcity' project)

--- .teamcity/settings.kts
+++ .teamcity/settings.kts
@@ -1,5 +1,6 @@
 import jetbrains.buildServer.configs.kotlin.*
 import jetbrains.buildServer.configs.kotlin.buildSteps.maven
+import jetbrains.buildServer.configs.kotlin.triggers.vcs
 
 /*
 The settings script is an entry point for defining a TeamCity
@@ -61,6 +62,11 @@ object Build : BuildType({
             }
             goals = "clean deploy"
             userSettingsSelection = "nexus-settings"
+        }
+    }
+
+    triggers {
+        vcs {
         }
     }
 })
